c1b8459bf02617554dd5eec3f07c5fb6d71862ab
carolinamatt <mhall@captechconsulting.com>
2017-10-24 17:28:13 -0400

    fix for adding blank movie causing crash

--- app/src/main/java/com/example/mhall/flickpicker/MainActivity.java
+++ app/src/main/java/com/example/mhall/flickpicker/MainActivity.java
@@ -22,40 +22,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String[] items={"No movies yet."};
+        String[] items = {"No movies yet."};
         movieList = new ArrayList<String>(Arrays.asList(items));
-        adapter=new ArrayAdapter<String>(this,R.layout.list,R.id.movieTextView,movieList);
-        listV = (ListView)findViewById(R.id.list);
+        adapter = new ArrayAdapter<String>(this, R.layout.list, R.id.movieTextView, movieList);
+        listV = (ListView) findViewById(R.id.list);
         listV.setAdapter(adapter);
-        inputBox = (EditText)findViewById(R.id.movieInput);
-        Button addMovieBtn =(Button)findViewById(R.id.addMovie);
+        inputBox = (EditText) findViewById(R.id.movieInput);
+        Button addMovieBtn = (Button) findViewById(R.id.addMovie);
 
         addMovieBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (movieList.get(0).contains("No movies yet")){
-                    movieList.remove(0);
-                }
                 String movie = inputBox.getText().toString();
-                // add new item to arraylist (as long as it's not blank)
                 if (!movie.equals("")) {
+                    if (movieList.get(0).contains("No movies yet")) {
+                        movieList.remove(0);
+                        adapter.notifyDataSetChanged();
+                    }
+                    // add new item to arraylist (as long as it's not blank)
                     movieList.add(movie);
                     // notify listview of data changed
                     adapter.notifyDataSetChanged();
 
                     //clear input box
                     inputBox.setText("");
+
                 }
             }
         });
     }
 
 
-    public void pickMovie(View v){
+    public void pickMovie(View v) {
         //check to see if movies have been added
         if (movieList.get(0).contains("No movies yet") || (movieList.size() <= 1)) {
             Toast.makeText(this, "Add movies first!", Toast.LENGTH_SHORT).show();
-            return;
         }
         int numMovies = movieList.size();
         int selectedMovie = (int) Math.floor(Math.random() * numMovies);
@@ -63,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
         displaySelectedMovie(selectedMovie);
     }
 
-    public void displaySelectedMovie(int selectedMovie){
+    public void displaySelectedMovie(int selectedMovie) {
         String selectedMovieName = movieList.get(selectedMovie);
         Toast.makeText(this, "FlickPicker selected " + selectedMovieName + ". Enjoy!", Toast.LENGTH_LONG).show();
 
